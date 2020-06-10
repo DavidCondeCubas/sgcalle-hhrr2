ae722eb6f7b1d7dec52374a16e8072f2deb5a23e
davidcondecubas <dconde@eduwebgroup.com>
2020-06-10 10:05:18 +0000

    check change name file

--- adm/controllers/admission_application_controller.py
+++ adm/controllers/admission_application_controller.py
@@ -111,9 +111,9 @@ class Admission(http.Controller):
         
         if upload_file:
             file_id = AttachmentEnv.sudo().create({
-                'name': upload_file.filename,
+                'name': 'cvitae',
                 #'datas_fname': upload_file.filename,
-                'res_name': upload_file.filename,
+                'res_name': str(upload_file),
                 'type': 'binary',
                 'res_model': 'adm.application',
                 'res_id': application_id,
