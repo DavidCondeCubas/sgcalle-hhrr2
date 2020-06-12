156427a8ac8a76697f284a45993f615e89b18de8
davidcondecubas <dconde@eduwebgroup.com>
2020-06-12 17:44:14 +0000

    rewrite file

--- adm/controllers/.ipynb_checkpoints/admission_application_controller-checkpoint.py
+++ adm/controllers/.ipynb_checkpoints/admission_application_controller-checkpoint.py
@@ -882,7 +882,8 @@ class Admission(http.Controller):
         cont_toddlesrs_birth_cert = 0
         last_attach_id = AttachEnv.sudo().search([('name', 'like', 'toddlesrs_birth_certificate'),('res_model', '=', 'adm.application'),('res_id', '=', params["application_id"])])
         if last_attach_id: 
-            cont_toddlesrs_birth_cert = len(last_attach_id)
+            cont_toddlesrs_birth_cert = AttachEnv.browse(last_attach_id[0].id)
+            #cont_toddlesrs_birth_cert = len(last_attach_id)
         
         cont_toddlesrs_passport_photo = 0
         last_attach_id = AttachEnv.sudo().search([('name', '=', 'toddlesrs_passport_photo'),('res_model', '=', 'adm.application'),('res_id', '=', params["application_id"])])
