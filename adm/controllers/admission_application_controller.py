b906125c63bf99fe4cc09f9eea5c2980776ea6b9
davidcondecubas <dconde@eduwebgroup.com>
2020-06-12 08:47:56 +0000

    rewrite file

--- adm/controllers/admission_application_controller.py
+++ adm/controllers/admission_application_controller.py
@@ -128,7 +128,20 @@ class Admission(http.Controller):
         
         AttachmentEnv = http.request.env["ir.attachment"]
         
+        attach_file  = -1
+        last_attach_id = AttachEnv.sudo().search([('name', '=', str(origin_nameFile)),('res_model', '=', 'adm.application'),('res_id', '=', params["application_id"])],order="create_date desc", limit=1)
+        #attach_file = AttachEnv.browse(AttachEnv.sudo().search([('res_model', '=', 'adm.application'),('res_id', '=', params["application_id"])])).ids
+        #attach_file = AttachEnv.browse([1027])
         if upload_file:
+            if last_attach_id: 
+                attach_file = AttachEnv.browse(last_attach_id[0].id)
+                attach_file.sudo().write({
+                            'res_id': application_id,
+                            'x_text': str(attach_file),
+                            #base64.b64encode(upload_file.read()),
+                            'datas': upload_file,
+                        })
+            else:
             file_id = AttachmentEnv.sudo().create({
                 'name': str(origin_nameFile),
                 #'datas_fname': upload_file.filename,
