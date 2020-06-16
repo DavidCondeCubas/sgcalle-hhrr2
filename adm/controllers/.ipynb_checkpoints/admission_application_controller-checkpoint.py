8a5b9246899e100500307141a31ff5480511ea55
DavidCondeCubas <dconde@eduwebgroup.com>
2020-06-16 06:49:53 +0000

    first version generate link enviando string como sudo

--- adm/controllers/.ipynb_checkpoints/admission_application_controller-checkpoint.py
+++ adm/controllers/.ipynb_checkpoints/admission_application_controller-checkpoint.py
@@ -1044,8 +1044,8 @@ class Admission(http.Controller):
                     'res_id':application.x_order_id.id,
                     'description': application.x_order_id.name,
                     'amount': application.x_order_id.amount_total,
-                    'currency_id': application.x_order_id.currency_id.id,
-                    'partner_id': application.partner_id.id,
+                    'currency_id': application.x_order_id.currency_id,
+                    'partner_id': application.partner_id,
                 })  
             wizard_data = WizardEnv.browse(created_wizard_id)
                 
